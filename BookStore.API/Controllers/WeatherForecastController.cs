9659332773b26d040748582e770d302b2ed53e9d
Jaimon <mc_@gmail.com>
2022-06-04 20:19:02 -0500

    Modified Sammuries

--- BookStore.API/Controllers/WeatherForecastController.cs
+++ BookStore.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BookStore.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild-1", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
